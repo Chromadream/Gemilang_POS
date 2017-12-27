f5971455e29ca8b48331d199a7c57098398f7bfa
Jonathan <jonicrecis@gmail.com>
2017-12-28 00:42:27 +1100

    login page

---
+++ login.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Login ke system</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+        <?php
+        if(empty($_POST["username"]))
+        {?>
+            <div class="container">
+                <form method="post">
+                    <div class="form-group">
+                      <label for="username"></label>
+                      <input type="text" class="form-control" id="username" aria-describedby="helpId" placeholder="username" required>
+                      <small id="helpId" class="form-text text-muted">Isi username</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="password"></label>
+                      <input type="password" class="form-control" id="password" placeholder="password" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        <?
+        }
+        else
+        {
+            include_once("database_class.php");
+            include_once("account_DAO.php");
+            $connection = new Database();
+            $accountDAO = new account_DAO($connection);
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $hashed_password = hash("sha256",$password);
+            $status = $accountDAO->login($username,$password);
+            if(!$status)
+            {
+                echo "Login details incorrect";
+            }
+            else
+            {
+                $direction = $_GET["redirposition"]
+                header("location: $direction");
+            }
+        }?>
+        
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
