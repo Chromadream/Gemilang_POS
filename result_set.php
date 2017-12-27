ff32254f7b1a998fe9b464a8b1eb05c636cd4efa
Jonathan <jonicrecis@gmail.com>
2017-12-27 22:58:05 +1100

    result set

---
+++ result_set.php
@@ -0,0 +1,26 @@
+<?php
+class result_set{
+    private $_results;
+
+    function __construct($results)
+    {
+        $this->_results = $results;
+    }
+
+    public function getNext($dataobject, $counter)
+    {
+        $row = $this->_results[$counter];
+        foreach($row as $key=>$value)
+        {
+            $dataobject->$key = $value;
+        }
+        return $dataobject;
+    }
+
+    public function rowCount()
+    {
+        $count = sizeof($this->_results);
+        return $count;
+    }    
+}
+?>
