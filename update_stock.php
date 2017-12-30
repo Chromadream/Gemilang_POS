9c69c343a5f4bb9b0e83c41a3f4a8e61c25e79c4
Jonathan Nicholas <jonicrecis@gmail.com>
2017-12-30 20:15:54 +1100

    update stock finished

---
+++ update_stock.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Update stock</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+      <?php
+      session_start();
+      if(!isset($_SESSION["level"]) || $_SESSION["level"] != "B" || $_SESSION["level"] != "S")
+      {
+        header("location: login.php?redirposition=update_stock.php");
+      }
+      include_once("database_class.php");
+      include_once("product_DAO.php");
+      $connection = new Database();
+      $product_DAO = new Product_DAO($connection);
+      if(empty($_POST["check"]))
+      {
+        $product_List = $product_DAO->list_all_product();?>
+        <form method="post" action="update_stock.php">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Stock tersedia</th>
+                    <th>Edit?</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+        for ($i = 0; $i<$product_List->rowCount();$i++)
+        {
+            $currentRow = $product_List->getNext(new Product_DAO($connection),$i);?>
+            <tr>
+              <td scope="row"><?php echo $currentRow->product_id; ?></td>
+              <td><?php echo $currentRow->product_name; ?></td>
+              <td><div class="form-group">
+                <label for=""></label>
+                <input type="text" class="form-control" name="<?php echo $currentRow->product_id; ?>" value="<?php echo $currentRow->product_stock_quantity;?>">
+              </div></td>
+              <td><div class="form-check">
+                  <input type="checkbox" class="form-check-input" name="" value="<?php echo $currentRow->product_id; ?>">
+              </div></td>
+            </tr>
+        <?php } ?>
+          </tbody>
+        </table>
+        <button type="submit" class="btn btn-primary">Update stock produk</button>    
+    <?php }
+    else
+    {
+        foreach($_POST["check"] as $id)
+        {
+            $product_DAO->update_product_stock($id,$_POST[$id]);
+            echo "Stock produk ".$id." telah terupdate dengan sukses";
+        }?>
+        <button type="button" class="btn btn-primary" btn-lg btn-block><a href="index.php">Kembali ke menu awal</a></button>
+        
+    <?php }?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
