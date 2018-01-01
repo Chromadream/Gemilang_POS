fa55424526bfe501f752a001e38692228725d704
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-01 18:39:41 +1100

    discount card registration

---
+++ new_discount_card.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Registrasi Kartu Diskon</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+      <?php
+      session_start();
+      if(!isset($_SESSION["level"]) || $_SESSION["level"] != "B")
+      {
+        header("location: login.php?redirposition=new_discount_card.php");
+      }
+      if(empty($_POST["discount_phone"]))
+      {?>
+        <div class="container">
+            <h1>Registrasi Kartu Diskon</h1>
+            <form action="new_discount_card.php" method="post">
+            <div class="form-group">
+              <label for="discount_phone">Nomor handphone</label>
+              <input type="text" class="form-control" name="discount_phone" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Tambah kartu diskon</button>
+            </form>
+        </div>  
+      <?php }
+      else
+      {
+        include_once("database_class.php");
+        include_once("discount_card_DAO.php");
+        $connection = new Database();
+        $discount_card_DAO = new discount_card_DAO($connection);
+        $discount_phone = $_POST["discount_phone"];
+        $id = $discount_card_DAO->new_discount_card($discount_phone);
+        echo "Kartu diskon telah berhasil ditambahkan. ID: ".$id;
+        echo '<button type="button" class="btn btn-primary" btn-lg btn-block><a href="index.php">Kembali ke menu awal</a></button>';
+      }?>.
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
