f9acc00c5e0407b405c93ffba99a6874253ae6fe
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-05 18:51:19 +1100

    uh yeah looks like I'm dumb

--- new_product.php
+++ new_product.php
@@ -16,7 +16,7 @@
            include_once("database_class.php");
             include_once("product_DAO.php");
             $connection = new Database();
-            $productDAO = new product_DAO($connection);
+            $productDAO = new product_DAO($connection->getConnection());
             $product_name = $_POST["product_name"];
             if(!empty($_POST["product_purchase_price"]))
             {
