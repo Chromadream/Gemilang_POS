69716e15e5715f9cc22cd640108c979322357eaa
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-06 20:04:23 +1100

    idk what went wrong

--- invoice.php
+++ invoice.php
@@ -28,7 +28,7 @@
     $transaction_DAO = new transaction_DAO($connection->getConnection());
     $item_DAO = new transaction_items_DAO($connection->getConnection());
     $customer_DAO = new customer_DAO($connection->getConnection());
-    krumo::get();
+    //krumo::get();
     krumo::session();
     if(!empty($_GET["mode"]))
     {
