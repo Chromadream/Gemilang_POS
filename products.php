4b3e81bcf02e6cd481225bfa8981742388724bda
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-07 23:58:57 +1100

    Revert "spacing issue on the products page due to shitty search implementation"
    This reverts commit 9609e43334ebb6576fdac50670310c0a1a693f0c.

--- products.php
+++ products.php
@@ -33,9 +33,8 @@
         }?>
         <div class="container">
         <h1>List Produk</h1>
-        <div class="container">
-            <a class="btn btn-primary" href="new_product.php" role="button"><i class="fa fa-plus" aria-hidden="true"></i> Produk baru</a>
-        </div>
+        <a class="btn btn-primary" href="new_product.php" role="button"><i class="fa fa-plus" aria-hidden="true"></i> Produk baru</a>
+        <br/>
         <form action="products.php" method="post">
             <div class="form-group">
               <input type="text" class="form-control" name="search" id="searchBar" aria-describedby="helpId" placeholder="Cari produk">
