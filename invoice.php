f1e87078f2808d80dfb585efa9fe533e8dfa6ec2
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-07 14:09:41 +1100

    script repositioning

--- invoice.php
+++ invoice.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="update_customer.js"></script>
+    <script src="change_quantity.js"></script>
+    <script src="remove_prod.js"></script>
   </head>
   <body>
     <?php
@@ -142,9 +145,7 @@
     </div>
 
     <!-- Optional JavaScript -->
-    <script src="update_customer.js"></script>
-    <script src="change_quantity.js"></script>
-    <script src="remove_prod.js"></script>
+    
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
