a3bf924ee9795e90eb74d37bb053c192756937b3
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-07 13:53:36 +1100

    backtracing

--- invoice.php
+++ invoice.php
@@ -81,6 +81,7 @@
                         <td><select name="customer" onchange="update_customer(<?php echo $id;?>)">
                             <?php
                             krumo::enable();
+                            krumo::backtrace();
                             krumo($customers->rowCount());
                             for ($i=0; $i<$customers->rowCount(); $i++)
                             {
