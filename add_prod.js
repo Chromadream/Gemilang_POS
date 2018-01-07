0b3bc7047bfc59dc3927d68ffd96afc6d402fc4f
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-07 23:05:21 +1100

    adding to invoice

---
+++ add_prod.js
@@ -0,0 +1,24 @@
+function add_prod(id,tid)
+{
+    try
+    {
+        var xmlhttp = new XMLHttpRequest();
+    }
+    catch(e)
+    {
+        alert("AJAX not supported, please use newer browser");
+        window.location.href = "https://google.com/chrome";
+    }
+
+    xmlhttp.open("GET","transact_wrapper.php?func=add_prod&id="+id+"&tid="+tid);
+    xmlhttp.onreadystatechange = triggered;
+    xmlhttp.send(null);
+
+    function triggered(){
+        result = xmlhttp.responseText;
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status==200))
+        {
+            window.location.href = "invoice.php?mode="+result;
+        }
+    }
+}
