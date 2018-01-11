c1022b5994fa9a3506368136aee6e72bf7a083f3
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-11 18:50:31 +1100

    discount card removal

--- discount_cards.php
+++ discount_cards.php
@@ -26,16 +26,20 @@
       $discount_card_DAO = new discount_card_DAO($connection->getConnection());
       krumo($discount_card_DAO);
       
-      krumo($result);?>
+      krumo($result);
+      if(empty($_POST["check"]))
+      {?>
       <div class="container">
           <h1>Daftar Kartu Diskon</h1>
           <a name="new_card" id="new_card" class="btn btn-primary" href="new_discount_card.php" role="button"><i class="fa fa-address-book" aria-hidden="true"></i>Registrasi Kartu Diskon Baru</a>
           <a name="home" id="home" class="btn btn-secondary" href="index.php" role="button"><i class="fa fa-home" aria-hidden="true"></i> Kembali ke menu awal</a>
+          <form action="discount_cards.php" method="post"></form>
           <table class="table">
               <thead>
                   <tr>
                       <th>ID</th>
                       <th>Nomor Telepon</th>
+                      <th>Hapus?</th>
                   </tr>
               </thead>
               <tbody>
@@ -48,11 +52,25 @@
               <tr>
                   <td scope="row"><?php echo $currentRow->discount_id;?></td>
                   <td><?php echo $currentRow->discount_phone;?></td>
+                <td><div class="form-check">
+                  <input type="checkbox" class="form-check-input" name="check[]" value="<?php echo $currentRow->discount_id; ?>">
+              </div></td>
               </tr>
           <?php };?>
               </tbody>
           </table>
+          <button type="submit" class="btn btn-success"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Hapus kartu diskon</button> 
       </div>
+          <?php }
+          else
+          {
+            foreach($_POST["check"] as $id)
+            {
+                $discount_card_DAO->remove_card($id);
+                echo "Kartu $id telah dihapus dari database<br />";
+            }?>
+        <a name="home" id="home" class="btn btn-secondary" href="index.php" role="button"><i class="fa fa-home" aria-hidden="true"></i> Kembali ke menu awal</a>
+          <?php } ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
