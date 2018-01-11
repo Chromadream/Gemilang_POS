edb9ac179b4201f0c4eebcb42664c86236f2aa04
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-11 20:39:03 +1100

    minorfix

--- products.php
+++ products.php
@@ -51,8 +51,8 @@
                     <th>Nama</th>
                     <th>Modal</th>
                     <th>Harga jual</th>
-                    <th>Satuan</th>
                     <th>Stock tersedia</th>
+                    <th>Satuan</th>
                     <th>Deskripsi</th>
                     <th>Edit?</th>
                 </tr>
