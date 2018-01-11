f2f02a35b5fa3386feceafde7ef71b3caacb1ae2
Jonathan Nicholas <jonicrecis@gmail.com>
2018-01-11 14:10:46 +1100

    discount card wrapping

---
+++ func/discount_wrapper.php
@@ -0,0 +1,42 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/database_class.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/DAO/transaction_DAO.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/DAO/discount_card_DAO.php");
+$connection = new Database();
+$transaction_DAO = new transaction_DAO($connection->getConnection());
+$discount_DAO = new discount_card_DAO($connection->getConnection());
+$mode = $_GET["mode"];
+$transactID = $_GET["tid"];
+$sentID = $_GET["id"];
+switch ($mode) {
+    case 'phone':
+        $ID = $discount_DAO->search_from_phone($sentID);
+        if($ID==NULL)
+        {
+            echo "Tidak ada kartu yang teregistrasi dengan nomor ini.<br/>";
+            echo "<a class='btn btn-link' href='invoice.php?mode=$transactID' role='button'>Kembali ke invoice</a>";
+            break;
+        }
+        else
+        {
+            $transaction_DAO->change_discount_code($transactID,$ID);
+            echo "Kartu diskon telah teregistrasi ke transaksi dengan sukses.<br/>";
+            echo "<a class='btn btn-link' href='invoice.php?mode=$transactID' role='button'>Kembali ke invoice</a>";
+            break;
+        }
+    case 'id':
+        $ID = $discount_DAO->search_from_id($sentID);
+        if($ID==NULL)
+        {
+            echo "Tidak ada kartu yang teregistrasi dengan ID ini.<br/>";
+            echo "<a class='btn btn-link' href='invoice.php?mode=$transactID' role='button'>Kembali ke invoice</a>";
+            break;
+        }
+        else
+        {
+            $transaction_DAO->change_discount_code($transactID,$ID);
+            echo "Kartu diskon telah teregistrasi ke transaksi dengan sukses.<br/>";
+            echo "<a class='btn btn-link' href='invoice.php?mode=$transactID' role='button'>Kembali ke invoice</a>";
+            break;
+        }
+}
---
+++ js/discount_wrapper.js
@@ -0,0 +1,51 @@
+function search_by_phone(tid)
+{
+    try
+    {
+        var xmlhttp = new XMLHttpRequest();
+    }
+    catch(e)
+    {
+        alert("AJAX not supported, please use newer browser");
+        window.location.href = "https://google.com/chrome";
+    }
+    let value = document.getElementById("phonenum").value;
+
+    xmlhttp.open("GET","func/discount_wrapper.php?mode=phone&id="+value+"&tid="+tid);
+    xmlhttp.onreadystatechange = triggered;
+    xmlhttp.send(null);
+
+    function triggered() {
+        result = xmlhttp.responseText;
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status==200))
+        {
+            document.getElementById("result").innerHTML = result;
+        }
+    }
+}
+
+function search_by_id(tid)
+{
+    try
+    {
+        var xmlhttp = new XMLHttpRequest();
+    }
+    catch(e)
+    {
+        alert("AJAX not supported, please use newer browser");
+        window.location.href = "https://google.com/chrome";
+    }
+    let value = document.getElementById("cardid").value;
+
+    xmlhttp.open("GET","func/discount_wrapper.php?mode=phone&id="+value+"&tid="+tid);
+    xmlhttp.onreadystatechange = triggered;
+    xmlhttp.send(null);
+
+    function triggered() {
+        result = xmlhttp.responseText;
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status==200))
+        {
+            document.getElementById("result").innerHTML = result;
+        }
+    }
+}
