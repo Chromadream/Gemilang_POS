e5b4e491272f77ce754a9af656d5f5fe2d7e8d1a
Jonathan Nicholas <jonicrecis@gmail.com>
2018-02-06 19:43:31 +1100

    work on wrapper and reporting page

---
+++ func/reporting_wrapper.php
@@ -0,0 +1,23 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/database_class.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/DAO/reporting_DAO.php");
+$connection = new Database();
+$reporting_DAO = new reporting_DAO($connection->getConnection());
+$functionality = $_GET["func"];
+$date = $_GET["d"];
+$month = $_GET["m"];
+$year = $_GET["y"];
+switch ($functionality) {
+    case 'yearly':
+        $result = $reporting_DAO->yearly_sum($year);
+        echo $result;
+        break;
+    case 'monthly':
+        $result = $reporting_DAO->monthly_sum($month,$year);
+        echo $result;
+        break;
+    case 'daily':
+        $result = $reporting_DAO->daily_sum($day,$month,$year);
+        echo $result;
+        break;
+}?>
