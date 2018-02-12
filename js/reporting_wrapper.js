6a71f76289fb19caf946a7897c97866a331ca965
Jonathan Nicholas <jonicrecis@gmail.com>
2018-02-12 22:45:08 +1100

    frontend work done

---
+++ js/reporting_wrapper.js
@@ -0,0 +1,113 @@
+
+//please don't change anything here, it just works
+function money_formatting(n){
+    var c = 2, 
+        t = ".", 
+        d = ",", 
+        s = n < 0 ? "-" : "", 
+        i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+        j = (j = i.length) > 3 ? j % 3 : 0;
+        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+function daily_report(day,month,year)
+{
+    try
+    {
+        var xmlhttp = new XMLHttpRequest();
+    }
+    catch(e)
+    {
+        alert("AJAX not supported, please use newer browser");
+        window.location.href = "https://google.com/chrome";
+    }
+
+    xmlhttp.open("GET","func/reporting_wrapper.php?func=daily&d="+day+"&m="+month+"&y"+year);
+    xmlhttp.onreadystatechange = triggered;
+    xmlhttp.send(null);
+    function triggered(){
+        result = xmlhttp.responseText;
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status==200))
+        {
+            document.getElementById("daily_sum").innerHTML = "Rp."+money_formatting(result);
+        }
+    } 
+}
+function monthly_report(month,year)
+{
+    try
+    {
+        var xmlhttp = new XMLHttpRequest();
+    }
+    catch(e)
+    {
+        alert("AJAX not supported, please use newer browser");
+        window.location.href = "https://google.com/chrome";
+    }
+
+    xmlhttp.open("GET","func/reporting_wrapper.php?func=monthly&m="+month+"&y"+year);
+    xmlhttp.onreadystatechange = triggered;
+    xmlhttp.send(null);
+    function triggered(){
+        result = xmlhttp.responseText;
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status==200))
+        {
+            document.getElementById("monthly_sum").innerHTML = "Rp."+money_formatting(result);
+        }
+    } 
+}
+
+function yearly_report(year){
+    try
+    {
+        var xmlhttp = new XMLHttpRequest();
+    }
+    catch(e)
+    {
+        alert("AJAX not supported, please use newer browser");
+        window.location.href = "https://google.com/chrome";
+    }
+
+    xmlhttp.open("GET","func/reporting_wrapper.php?func=yearly&y"+year);
+    xmlhttp.onreadystatechange = triggered;
+    xmlhttp.send(null);
+    function triggered(){
+        result = xmlhttp.responseText;
+        if ((xmlhttp.readyState == 4) && (xmlhttp.status==200))
+        {
+            document.getElementById("yearly_sum").innerHTML = "Rp."+money_formatting(result);
+        }
+    } 
+}
+
+function yearcalendar(endYear,onChangeFunction,currentYear){
+    var start = "<select id='yearselector"+onChangeFunction+"'>"
+    for (let index = 2018; index <= endYear; index++) {
+        var selected = index ===currentYear ? "selected" : null;
+        start += "<option value="+index+" "+selected+">"+index+"</option>"
+    }
+    return start;
+}
+
+function monthcalendar(onchangefunction,currentmonth)
+{
+    var start = "<select id='monthselector"+onchangefunction+"'>"
+    for (let index = 1; index <= 12; index++) {
+        var selected = index === currentmonth ? "selected" : null;
+        start+="<option value="+index+" "+selected+">"+index+"</option>"
+    }
+    return start;
+}
+
+window.onload = function () {
+    const todaydate = new Date();
+    const year = todaydate.getFullYear();
+    const month = todaydate.getMonth()+1;
+    const day = todaydate.getDate();
+    document.getElementById("yearreporting").innerHTML = yearcalendar(year,"yearreport",year);
+    document.getElementById("monthreporting").innerHTML = yearcalendar(year,"monthreport",year)+monthcalendar("monthreport",month);
+    document.getElementById("dailypicker").value = todaydate.toISOString().substring(0,10);
+    daily_report(day,month,year);
+    monthly_report(month,year);
+    yearly_report(year);
+}
--- reporting.php
+++ reporting.php
@@ -10,12 +10,40 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
+  <?php
+  if(!isset($_SESSION["level"]) || $_SESSION["level"] != "B")
+  {
+    header("location: login.php?redirposition=invoice.php?mode=".$_GET["mode"]);
+  }
+  else
+  { ?>
   <body>
-      
+      <table class="table table-inverse table-responsive">
+          <tbody>
+            <tr>
+              <td scope="row">Harian</td>
+              <td><input type="date" id="dailypicker" /></td>
+              <td><div id="daily_sum"></div></td>
+            </tr>
+            <tr>
+              <td scope="row">Bulanan</td>
+              <td><div id="monthreporting"></div></td>
+              <td><div id="monthly_sum"></div></td>
+            </tr>
+            <tr>
+              <td scope="row">Tahunan</td>
+              <td><div id="yearreporting"></div></td>
+              <td><div id="yearly_sum"></div></td>
+            </tr>
+          </tbody>
+      </table>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
   </body>
+  <?php
+  }
+  ?>
 </html>
