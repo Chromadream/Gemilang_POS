83cd4fc90fa1c22cf56bed66954997f35a6fd9c7
Jonathan Nicholas <jonicrecis@gmail.com>
2018-02-22 13:40:37 +1100

    index.php Fontawesome5 migration

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   </head>
   <body>
         <div class="container">
@@ -21,23 +22,24 @@
         }
         if($_SESSION["level"]=="B" || $_SESSION["level"]=="T")
         {?>
-            <a name="invoice" id="invoice" class="btn btn-link" href="invoice.php?mode=new" role="button"><i class="fa fa-exchange" aria-hidden="true"></i> Kasir</a>
+            <a name="invoice" id="invoice" class="btn btn-link" href="invoice.php?mode=new" role="button"><i class="fas fa-exchange-alt"></i> Kasir</a>
             <br/>
         <?php }
         if($_SESSION["level"]=="S")
         {?>
-            <a name="inventory" id="inventory" class="btn btn-link" href="inventory.php" role="button"><i class="fa fa-archive" aria-hidden="true"></i> Stock</a>
+            <a name="inventory" id="inventory" class="btn btn-link" href="inventory.php" role="button"><i class="fas fa-archive"></i> Stock</a>
             <br/>
         <?php }
         if($_SESSION["level"]=="B")
         {?>
-            <a name="product" id="product" class="btn btn-link" href="products.php" role="button"><i class="fa fa-archive" aria-hidden="true"></i> Produk</a><br/>
-            <a name="order" id="order" class="btn btn-link" href="orders.php" role="button"><i class="fa fa-line-chart" aria-hidden="true"></i> Transaksi</a><br/>
-            <a name="cust" id="cust" class="btn btn-link" href="customers.php" role="button"><i class="fa fa-user" aria-hidden="true"></i> Pelanggan</a><br/>
-            <a name="disc" id="disc" class="btn btn-link" href="discount_cards.php" role="button"><i class="fa fa-id-card-o" aria-hidden="true"></i> Kartu diskon</a><br/>
+            <a name="product" id="product" class="btn btn-link" href="products.php" role="button"><i class="fas fa-archive"></i> Produk</a><br/>
+            <a name="order" id="order" class="btn btn-link" href="orders.php" role="button"><i class="fas fa-chart-line"></i> Transaksi</a><br/>
+            <a name="reporting" id="reporting" class="btn btn-link" href="reporting.php" role="button"><i class="fas fa-book"></i> Laporan</a><br/>
+            <a name="cust" id="cust" class="btn btn-link" href="customers.php" role="button"><i class="fas fa-user"></i> Pelanggan</a><br/>
+            <a name="disc" id="disc" class="btn btn-link" href="discount_cards.php" role="button"><i class="fas fa-id-card"></i> Kartu diskon</a><br/>
         <?php }
         ?>
-            <a name="logout" id="logout" class="btn btn-link" href="logout.php" role="button"><i class="fa fa-sign-out" aria-hidden="true"></i> Keluar</a><br/>
+            <a name="logout" id="logout" class="btn btn-link" href="logout.php" role="button"><i class="fas fa-sign-out"></i> Keluar</a><br/>
 
         </div>
     <!-- Optional JavaScript -->
