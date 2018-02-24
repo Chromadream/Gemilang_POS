371b13e3a08ac37898790ec09c7f380abf123ff0
Jonathan Nicholas <jonicrecis@gmail.com>
2018-02-24 16:52:39 +1100

    hack details

--- js/reporting_wrapper.js
+++ js/reporting_wrapper.js
@@ -9,6 +9,10 @@ function money_formatting(n){
         j = (j = i.length) > 3 ? j % 3 : 0;
         return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
+
+//CPU-heavy
+//kills the CPU even on i7-6700hq
+//only usable on sub 100ms sleep
 function sleep(delay) {
     var start = new Date().getTime();
     while (new Date().getTime() < start + delay);
