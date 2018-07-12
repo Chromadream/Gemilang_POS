7b931ba55394cc1fc2b4fb01a71f22f076c344b2
Jonathan Nicholas <jonicrecis@gmail.com>
2018-07-12 16:40:21 +0700

    TCPDF generator test

--- print_generator.php
+++ print_generator.php
@@ -1,7 +1,24 @@
 <?php
 include_once("vendor/autoload.php");
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Test</h1>');
-$mpdf->Output('test.pdf',\Mpdf\Output\Destination::INLINE);
-
+define('K_PATH_IMAGES','img/');
+$PDF_HEADER_LOGO = "logo.jpg";
+$PDF_HEADER_LOGO_WIDTH = "50";
+$sizes = array(215,140);
+$tcpdf = new TCPDF('L','mm',$sizes,true,'UTF-8',false,false);
+$tcpdf->SetHeaderData($PDF_HEADER_LOGO,$PDF_HEADER_LOGO_WIDTH,"Invoice",'');
+$tcpdf->SetHeaderFont(array(PDF_FONT_NAME_MAIN,'',20));
+$tcpdf->SetFooterFont(array(PDF_FONT_NAME_DATA,'',PDF_FONT_SIZE_DATA));
+$tcpdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED,'',12);
+$tcpdf->SetMargins(PDF_MARGIN_LEFT,PDF_MARGIN_TOP,PDF_MARGIN_RIGHT);
+$tcpdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$tcpdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$tcpdf->SetAutoPageBreak(FALSE,PDF_MARGIN_BOTTOM);
+$tcpdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+$tcpdf->AddPage();
+$tcpdf->writeHTML("<h1>test</h1>",true,true,true,true,'');
+$savedir = $_SERVER["DOCUMENT_ROOT"]."/tmps/";
+if($tcpdf->Output('testoutput','I')){
+    return "<h1>test</h1>";
+}
+exit();
 ?>
